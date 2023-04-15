81a40f2ec14f4a73f2e69097ca9d44d926ddc689
Praduman03 <praduman03k@gmail.com>
2023-04-15 10:13:30 +0530

    fixed minor typos

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cors("https://versely-go.onrender.com/getposts"));
+app.use(cors());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
